a30fe595f978489719617f228a77cf76d3283783
khurshedansari0786 <khurshedansari12403@gmail.com>
2025-08-13 09:42:57 +0530

    Initial commit

---
+++ server.js
@@ -0,0 +1,81 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const path = require('path');
+
+const app = express();
+
+// MongoDB Connection......
+
+mongoose.connect("mongodb://127.0.0.1:27017/suportSystem")
+  .then(() => {
+    console.log("MongoDB Connected");
+    console.log("Models registered:", mongoose.modelNames()); // 👈 Check this
+  })
+  .catch(err => console.error("MongoDB Error:", err));
+
+// Models
+const Register = require("./models/Register");
+const Contact = require("./models/Contact");
+const Donation = require("./models/Donation");
+
+// Middleware
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static(path.join(__dirname, "public")));
+
+// Register Route
+app.post("/api/register", async (req, res) => {
+  try {
+    console.log("Received Register Data:", req.body); //show data in console
+    const { name, email, password } = req.body;
+    await Register.create({ name, email, password });
+   res.status(200).json({ message: 'Registration Successfully!'});
+  } catch (err) {
+    console.error("Registration Error:", err);
+    res.status(500).send("Registration Failed");
+  }
+});
+
+// Contact Route
+app.post("/api/contact", async (req, res) => {
+  try {
+    console.log("Received Contact Data:", req.body); // show data in console
+    const { name, email, message } = req.body;
+    await Contact.create({ name, email, message });
+   res.status(200).json({ message: 'Contact form Submitted!' });
+  } catch (err) {
+    console.error("Contact Error:", err);
+    res.status(500).send("Contact Submission Failed");
+  }
+});
+
+
+// Donation Route
+app.post("/api/donation", async (req, res) => {
+  try {
+    console.log("Received Donation Data:", req.body);
+
+    const { donorName, donationAmount, donationMessage } = req.body;
+
+    await Donation.create({ donorName, donationAmount, donationMessage });
+
+    // ✅ Payment link generate
+    const upiID = "khurshedansari12403@okhdfcbank"; // apna UPI ID dalen
+    const paymentLink = `upi://pay?pa=${upiID}&pn=${encodeURIComponent(donorName)}&am=${donationAmount}&cu=INR`;
+
+    res.status(200).json({
+      success: true,
+      paymentLink
+    });
+
+  } catch (err) {
+    console.error("Donation Error:", err);
+    res.status(500).send("Donation Failed");
+  }
+});
+
+
+// Server Listen
+app.listen(3000, () => {
+  console.log(`server listening at http://localhost:3000`);
+});
